c7e21c67adc4d2db4fcb02864b41fe6ef284c562
abby-shabi <abbyshabi@gmail.com>
2019-02-03 05:42:03 +0300

    added a test file for source class

--- app/news_test.py
+++ app/news_test.py
@@ -2,3 +2,19 @@ import unittest
 from models import source
 
 Sources = source.Sources
+
+class SourceTest(unittest.TestCase):
+    '''
+    Test class to test the behaviour of the source class
+    '''
+    def setUp(self)
+    '''
+    set up method that will run before every test 
+    '''
+    self.new_source = Sources(12,'abc','abcdef','ab@ab.com','abc','au')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_source,Sources))
+
+if __name__ == '__main__':
+    unittest.main()
