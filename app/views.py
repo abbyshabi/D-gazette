cbb82c27ded34cac758c3c7d2931bb53afe4270a
abby-shabi <abbyshabi@gmail.com>
2019-02-03 05:24:30 +0300

    created configuration classes

--- app/views.py
+++ app/views.py
@@ -14,8 +14,18 @@ Articles= articles.Articles
 def index():
 
     '''
-    View root page function that returns the index page and its data
+    The root view for the index page
     '''
     title = "Home - Welcome to D-Gazette"
     news_sources = get_sources('sources')
     return render_template('index.html',title = title,news_sources = news_sources)
+
+@app.route('/article/<source_id>')
+def source(source_id):
+    '''
+    The view page for the news page
+    '''
+
+    
+    source_and_articles = get_source_articles(source_id)
+    return render_template('news.html', source_and_articles=source_and_articles)
