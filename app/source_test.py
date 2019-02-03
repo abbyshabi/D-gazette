6bb002bdd482179263da83d9a605f4a74cf49156
abby-shabi <abbyshabi@gmail.com>
2019-02-03 05:48:51 +0300

    added the article class

--- app/source_test.py
+++ app/source_test.py
@@ -1,17 +1,17 @@
 import unittest
 from models import source
 
-Sources = source.Sources
+Sources=source.Sources
 
 class SourceTest(unittest.TestCase):
     '''
     Test class to test the behaviour of the source class
     '''
-    def setUp(self)
-    '''
-    set up method that will run before every test 
-    '''
-    self.new_source = Sources(12,'abc','abcdef','ab@ab.com','abc','au')
+    def setUp(self):
+        '''
+        set up method that will run before every test 
+        '''
+        self.new_source = Sources(12,'abc','abcdef','ab@ab.com','abc','au')
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_source,Sources))
